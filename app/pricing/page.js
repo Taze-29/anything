fe9dd98354d2c3eecfc5e8fdbcee0e7b65a3f62a
Taze-29 <matteozagaroli29@gmail.com>
2025-01-15 18:22:52 +0000

    new price page

---
+++ app/pricing/page.js
@@ -0,0 +1,13 @@
+import Nav from "@/components/Nav";
+import Faq from "@/components/Faq";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+    return (
+      <>
+      <Nav />
+      <Faq />
+      <Footer />
+      </>
+    );
+  }
---
+++ components/PricingHero.js
@@ -0,0 +1,17 @@
+const PricingHero = () => {
+
+    return (
+      <div className= "">
+        <div className="">
+          <h1>
+            Choose the Perfect Plan<br />
+            For Your Business<br />
+            At the Right Price
+          </h1>
+        </div>
+        <div className=""></div>
+      </div>
+    )
+  }
+  
+  export default PricingHero;
