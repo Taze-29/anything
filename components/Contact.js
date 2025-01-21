27267e4b2015268a6a5eafa81dca9bfebbb4cc88
Taze-29 <matteozagaroli29@gmail.com>
2025-01-21 15:56:14 +0000

    latest

--- components/Contact.js
+++ components/Contact.js
@@ -4,19 +4,19 @@ const Contact = () => {
 
     return (
     <div className="h-screen flex relavtive justify-between md:px-[8rem]">
-        <div className="flex flex-col w-[35%] gap-8 ">
+        <div className="flex flex-col w-[35%] gap-8 h-[70%] justify-center">
             <p className='md:text-2xl'>Contact Us</p>
             <h2 className='md:text-6xl'>Your project<br /> Take the next step</h2>
             <p className=''>Send us an email and let us know everything you want out of a new site. We respond to everyone within 24 hours of contacting us. Or give us a call and reach us immediately for a chat. If we are unavailable we will call back same day.</p>
         </div>
         <form className="w-[45%]">
-            <div className="flex flex-col">
+            <div className="flex flex-col h-[100%] justify-center gap-20">
                 <div >
                     <input 
                         type="text" 
                         id="name" 
                         name="name" 
-                        className="w-full p-2 border-b-2 border-gray-300 bg-inherit focus:outline-none required:border-b-red-500"
+                        className="w-full md:text-xl p-2 border-b-2 border-gray-300 bg-inherit focus:outline-none required:border-b-red-500"
                         placeholder="Name *"
                     />
                 </div>
@@ -25,7 +25,7 @@ const Contact = () => {
                         type="email" 
                         id="email" 
                         name="email" 
-                        className="w-full p-2 border-b-2 border-gray-300 bg-inherit focus:outline-none"
+                        className="w-full md:text-xl p-2 border-b-2 border-gray-300 bg-inherit focus:outline-none"
                         placeholder="Email *"
                     />
                 </div>
@@ -34,7 +34,7 @@ const Contact = () => {
                         type="tel" 
                         id="phone" 
                         name="phone" 
-                        className="w-full p-2 border-b-2 border-gray-300 bg-inherit focus:outline-none"
+                        className="w-full md:text-xl p-2 border-b-2 border-gray-300 bg-inherit focus:outline-none"
                         placeholder="Phone Number *"
                     />
                 </div>
@@ -43,11 +43,11 @@ const Contact = () => {
                         id="message" 
                         name="message" 
                         rows="1" 
-                        className="w-full p-2 border-b-2 border-gray-300 bg-inherit focus:outline-none"
+                        className="w-full md:text-xl p-2 border-b-2 border-gray-300 bg-inherit focus:outline-none"
                         placeholder="Message"
                     ></textarea>
                 </div>
-                <div className="flex items-center space-x-2">
+                <div className="flex items-center space-x-4">
                     <input 
                         type="checkbox" 
                         id="accept-privacy" 
@@ -61,10 +61,10 @@ const Contact = () => {
                         I accept the <a href="/privacy-policy" className=" underline">Privacy Policy</a>
                     </label>
                 </div>
-                <div>
+                <div className='flex justify-end'>
                     <button 
                         type="submit" 
-                        className="border-2 border-black rounded-full py-1 px-2 hover:bg-black hover:text-white transition-all duration-300 "
+                        className="border-2 md:text-xl border-black rounded-full py-2 px-4 hover:bg-black hover:text-white transition-all duration-300 "
                     >
                         Submit
                     </button>
