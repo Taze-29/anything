ea929c2ad4e41e6f50fa2b4174981fadf9de39b0
jp010263 <c.hope@student.reading.ac.uk>
2025-01-21 12:51:57 +0000

    threejs test

--- app/services/page.js
+++ app/services/page.js
@@ -1,19 +1,26 @@
 import ServicesLarge from "@/components/ServicesLarge";
 import Workflow from "@/components/Workflow";
 import CTABanner from "@/components/CTABanner";
+import FloatingShapes from "@/components/FloatingShapes/FloatingShapes";
 
 export default function Home() {  
 
   return (
     <>
-    <div className="md:px-32">
-      {/* Hero */}
-      <div className="h-[100vh] flex flex-col justify-center items-center -mt-24">
+    <section className="w-full h-[100vh] -mt-28">
+      <div className="absolute z-10 w-full h-full flex justify-center items-center">              
         <h1 className="text-9xl font">SERVICES</h1>
       </div>
-
+      <FloatingShapes />
+    </section>
+    <div className="md:px-32">
+      {/* Hero */}      
+      <section>
       <ServicesLarge />
+      </section>
+      <section>
       <CTABanner />
+      </section>      
       <Workflow />
       
     </div>
